7bc3c3cae282e107eb6ac1a68acef928fb7ac968
Juan Garcia <juagarca@gmail.com>
2021-01-18 19:58:18 +0000

    added nokogiri to gemfile

--- website.rb
+++ website.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'pry-byebug'
 require 'better_errors'
-# require 'nokogiri'
+require 'nokogiri'
 require 'open-uri'
 
 configure :development do
@@ -12,27 +12,27 @@ end
 
 get '/' do
   # Lastfm url to scrape
-  # url = 'https://www.last.fm/user/Darin86'
-  # html_content = open(url).read
-  # doc = Nokogiri::HTML(html_content)
+  url = 'https://www.last.fm/user/Darin86'
+  html_content = open(url).read
+  doc = Nokogiri::HTML(html_content)
 
-  # @songs = []
-  # # Scanning first 5 songs
-  # doc.search('.chartlist-row').first(5).each do |row|
-  #   title = row.at('.chartlist-name').children[1].attributes['title'].value
-  #   artist = row.at('.chartlist-artist').children[1].attributes['title'].value
-  #   time = row.at('.chartlist-timestamp').children[1].children.text.strip
-  #   time = 'Listening now...' if time.include?('Scrobbling')
+  @songs = []
+  # Scanning first 5 songs
+  doc.search('.chartlist-row').first(5).each do |row|
+    title = row.at('.chartlist-name').children[1].attributes['title'].value
+    artist = row.at('.chartlist-artist').children[1].attributes['title'].value
+    time = row.at('.chartlist-timestamp').children[1].children.text.strip
+    time = 'Listening now...' if time.include?('Scrobbling')
 
-  #   image = row.at('.chartlist-image').children[1].children[1].attributes['src'].value
-  #   # Passing songs to the view
-  #   @songs << {
-  #     title: title,
-  #     artist: artist,
-  #     time: time,
-  #     image: image
-  #   }
-  # end
+    image = row.at('.chartlist-image').children[1].children[1].attributes['src'].value
+    # Passing songs to the view
+    @songs << {
+      title: title,
+      artist: artist,
+      time: time,
+      image: image
+    }
+  end
   erb :index
 end
 
